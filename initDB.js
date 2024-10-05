3e5a698fa882a6c6f311cf57e5016cdd529b03f8
Yogesh Jaga <yogeshjaga123@gmail.com>
2024-10-05 12:57:19 +0530

    added correct path in initDB.js file

--- initDB.js
+++ initDB.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.join('/tmp', 'database.sqlite');
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./database.sqlite', (err) => {
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
